24a631fb6ace837ab6d5609b7928979a10308b4e
tgoddessana <twicegoddessana1229@gmail.com>
2023-02-09 01:00:23 +0900

    FEAT : request 에 포함되어 있는 access token을 Blacklist 에 추가 (로그아웃 기능 구현중)

--- pay_right_here/jwtauth/urls.py
+++ pay_right_here/jwtauth/urls.py
@@ -4,7 +4,10 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from pay_right_here.jwtauth.views import jwtauth_logout
+
 urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="jwtauth_login"),
     path("refresh/", TokenRefreshView.as_view(), name="jwtauth_refresh"),
+    path("logout/", jwtauth_logout, name="jwtauth_logout"),
 ]
--- pay_right_here/jwtauth/views.py
+++ pay_right_here/jwtauth/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from pay_right_here.jwtauth.jwt import AccessToken
+
+
+@api_view(("POST",))
+def jwtauth_logout(request):
+    """access token을 Blacklist 에 추가합니다."""
+    access_token = AccessToken(token=request.headers.get("Authorization").split(" ")[1])
+    access_token.blacklist()
+    return Response(status=status.HTTP_204_NO_CONTENT)
