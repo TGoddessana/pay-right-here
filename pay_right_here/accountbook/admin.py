dcf88887bcd33f7704dcd5f276ca80a06c6674f0
tgoddessana <twicegoddessana1229@gmail.com>
2023-02-09 03:02:26 +0900

    FEAT : 가계부, 작성내역 모델 작성 및 마이그레이션

--- pay_right_here/accountbook/admin.py
+++ pay_right_here/accountbook/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from pay_right_here.accountbook.models import AccountBook, AccountBookHistory
 
-# Register your models here.
+
+@admin.register(AccountBook)
+class AccountBookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AccountBookHistory)
+class AccountBookHistoryAdmin(admin.ModelAdmin):
+    pass
--- pay_right_here/accountbook/models.py
+++ pay_right_here/accountbook/models.py
@@ -1,3 +1,50 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from pay_right_here.accounts.models import User
+
+
+class TimestampedModel(models.Model):
+    """모델의 생성일자와 수정일자를 저장합니다. abstract=True 이므로 실제 데이터베이스에는 테이블이 생기지 않습니다."""
+
+    class Meta:
+        abstract = True
+
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+
+class AccountBook(TimestampedModel):
+    """
+    가계부 모델입니다.
+    각각의 필드는 아래의 의미를 가집니다.
+
+    - uuid : 가계부 한 권에 대한 유일한 uuid
+    - user : 가계부 작성자
+    - title : 가계부 제목
+    """
+
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"<{self.title} | {self.user.name}>"
+
+
+class AccountBookHistory(TimestampedModel):
+    """
+    작성내역 모델입니다.
+    각각의 필드는 아래의 의미를 가집니다.
+
+    - accountbook : 가계부
+    - memo : 메모
+    - amount : 금액, - 값일 경우 소비를, + 값일 경우 생산을 의미
+    """
+
+    accountbook = models.ForeignKey(AccountBook, on_delete=models.CASCADE)
+    memo = models.CharField(max_length=150)
+    amount = models.BigIntegerField()
+
+    def __str__(self):
+        return f"<{self.memo} | {self.amount}>"
