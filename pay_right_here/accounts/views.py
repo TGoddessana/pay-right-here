1e8092a06a7d39c2e41b978ff1f738af926fb0f8
tgoddessana <twicegoddessana1229@gmail.com>
2023-02-08 22:56:29 +0900

    FEAT : 회원가입 요청은 누구나 가능하도록 변경

--- pay_right_here/accounts/views.py
+++ pay_right_here/accounts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, status
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 
 from pay_right_here.accounts.models import User
@@ -8,6 +8,7 @@ from pay_right_here.accounts.serializers import RegisterSerializer, WithdrawSeri
 class RegisterView(generics.CreateAPIView):
     """회원가입을 처리하는 View 입니다. POST 메서드만 허용됩니다."""
 
+    permission_classes = [permissions.AllowAny]
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
 
