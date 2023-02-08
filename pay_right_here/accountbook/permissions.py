068764ba9eb4dda81eb174478175c10099b20951
tgoddessana <twicegoddessana1229@gmail.com>
2023-02-09 07:01:00 +0900

    FEAT : 가계부 작성내역에 대한 권한 설정

---
+++ pay_right_here/accountbook/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework import permissions
+from rest_framework.generics import get_object_or_404
+
+from pay_right_here.accountbook.models import AccountBook, AccountBookHistory
+
+
+class AccountBookHistoryPermission(permissions.IsAuthenticated):
+    """가계부를 다루려면 로그인한 상태에서 다루어야 하고, 본인이 작성한 가계부만 수정하거나, 삭제하거나, 조회할 수 있습니다."""
+
+    def has_permission(self, request, view):
+        """요청한 사용자의 id가, kwargs로 특정할 수 있는 AccountBook 의 저자의 id와 같은지를 반환합니다."""
+        # 가계부 인스턴스가 존재하지 않으면, 404 Error
+        accountbook_id = view.kwargs.get("pk")
+        get_object_or_404(AccountBook, id=accountbook_id)
+        return (
+            request.user.id == AccountBook.objects.get(id=view.kwargs.get("pk")).user.id
+        )
